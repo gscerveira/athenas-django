8050784ccbe9db1c9b89067d9b5ee3a2401cc2d3
gscerveira <61796134+gscerveira@users.noreply.github.com>
2024-09-09 18:18:17 -0300

    Correção

--- backend/urls.py
+++ backend/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('api.urls')),
+    path('', include('backend.api.urls')),
     path('admin/', admin.site.urls),
 ]
