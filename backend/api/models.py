b6d2dc67237ae1360fa6700713a15d11e057b557
gscerveira <61796134+gscerveira@users.noreply.github.com>
2024-09-08 23:56:17 -0300

    Definição do modelo da entidade Pessoa

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Pessoa(models.Model):
+    nome = models.CharField(max_length=100)
+    data_nasc = models.DateField()
+    cpf = models.CharField(max_length=11)
+    sexo = models.CharField(max_length=1)
+    altura = models.DecimalField(max_digits=3, decimal_places=2)
+    peso = models.DecimalField(max_digits=5, decimal_places=2)
